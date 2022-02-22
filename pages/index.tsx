38955c421641afb24dd5a05aecc725219c9d4afb
Jacob Juul Larsen <jjl@ambition.dk>
2022-02-22 11:22:57 +0100

    added home contact icons and reorganized styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
+
 import Head from 'next/head'
+import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -11,13 +14,19 @@ const Home: NextPage = () => {
 		</Head>
 
 		<div className={styles.container}>
-
 			<div className={styles.headlines}>
 				<h1>Jacob Juul Larsen</h1>
 				<h2>Web developer</h2>
 			</div>
 
-			
+			<div className={styles.contact}>
+				<a target="_blank" href="mailto:jacoblarsen86@live.com" rel="noopener noreferrer">
+					<Image alt="email me" src='/icon-envelope.svg' width="56" height="40"/>
+				</a>
+				<a target="_blank" href="https://www.linkedin.com/in/larsenjacob/" rel="noopener noreferrer">
+					<Image alt="linkedin" src='/icon-linkedin.svg' width="43.2" height="40"/>
+				</a>
+			</div>		
 		</div>
 	</>
 }
--- pages/work.tsx
+++ pages/work.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 import Head from 'next/head'
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Work.module.css'
 
 import UnderlinedTitle from '../components/UnderlinedTitle'
 import Timeline from '../components/Timeline'
