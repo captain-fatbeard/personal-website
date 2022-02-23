3f1066d884c8aa8842fea127eb5af60c00519e03
jacob juul larsen <jjl@ambition.dk>
2022-02-23 09:55:04 +0100

    moved timeline events from component to work page

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -2,37 +2,18 @@ import { motion } from 'framer-motion'
 
 import styles from './styles.module.css'
 
-const Timeline: React.FC = () => {
+interface Event {
+    title: string
+    text: string
+    present?: boolean
+    year: string
+}
 
-    const events: {present?: boolean, year: string, title: string, text: string}[] = [
-        {
-            present: true,
-            year: '2020',
-            title: 'Ambition',
-            text:  'Tech Lead; Laravel, javascript, react, GCP, kubernetes'
-        },
-        {
-            year: '2015 - 2019',
-            title: 'Netbooster / Artefact',
-            text:  'Team lead; PHP, javascript, vuejs'
-        },
-        {
-            year: '2013 - 2015',
-            title: 'Designheroes',
-            text:  'PHP Frameworks, webshops'
-        },
-        {
-            year: '2012',
-            title: 'Rete-Moto / Heroes & Generals',
-            text:  'Web-dev & forum maintenance. Started as 2 month internship.'
-        },
-        {
-            year: '2011',
-            title: 'AmPd',
-            text:  'Freelance Web development, design and programming in HTML5/CSS3.'
-        },
-    ]
+interface Props {
+    events: Array<Event>
+}
 
+const Timeline: React.FC<Props> = ({ events }) => {
 	return (
         <div className={styles.timeline}>
             {events.map(event => (
--- pages/work.tsx
+++ pages/work.tsx
@@ -8,6 +8,35 @@ import UnderlinedTitle from '../components/UnderlinedTitle'
 import Timeline from '../components/Timeline'
 
 const Home: NextPage = () => {
+    const events: {present?: boolean, year: string, title: string, text: string}[] = [
+        {
+            present: true,
+            year: '2020',
+            title: 'Ambition',
+            text:  'Tech Lead; Laravel, javascript, react, GCP, kubernetes'
+        },
+        {
+            year: '2015 - 2019',
+            title: 'Netbooster / Artefact',
+            text:  'Team lead; PHP, javascript, vuejs'
+        },
+        {
+            year: '2013 - 2015',
+            title: 'Designheroes',
+            text:  'PHP Frameworks, webshops'
+        },
+        {
+            year: '2012',
+            title: 'Rete-Moto / Heroes & Generals',
+            text:  'Web-dev & forum maintenance. Started as 2 month internship.'
+        },
+        {
+            year: '2011',
+            title: 'AmPd',
+            text:  'Freelance Web development, design and programming in HTML5/CSS3.'
+        },
+    ]
+
 	return <>
 		<Head>
 			<title>Jacob Juul Larsen - work</title>
@@ -17,7 +46,7 @@ const Home: NextPage = () => {
 
 		<div className={styles.container}>
 			<UnderlinedTitle title="work"/>
-			<Timeline />
+			<Timeline events={events} />
 		</div>
 	</>
 }
