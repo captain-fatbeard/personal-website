2a2c233b5c1b8e9bef885bbc34a5276a8268c4c4
jacob juul larsen <jjl@ambition.dk>
2022-02-23 11:04:11 +0100

    added skills Graph

---
+++ components/Graph/index.tsx
@@ -0,0 +1,31 @@
+import { motion } from 'framer-motion'
+
+import styles from './styles.module.css'
+
+interface Skill {
+    title: string
+    level: number
+}
+
+interface Props {
+    skillset: Array<Skill>
+}
+
+const Graph: React.FC<Props> = ({ skillset }) => {
+	return (
+        <div className={styles.container}>
+            {skillset.map(skill => (
+                <motion.div
+                    key={skill.title}
+                    className={styles.bar}
+                    animate={{ height: `${skill.level}%` }}
+                    transition={{ duration: Math.random() * (2 - 0.3) + 0.3 }}
+                >
+                    {skill.title}
+                </motion.div>         
+            ))}
+        </div>
+    )
+}
+
+export default Graph
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,25 +1,46 @@
+import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
-
 import Head from 'next/head'
 
 import styles from '../styles/Skills.module.css'
 
 import UnderlinedTitle from '../components/UnderlinedTitle'
+import Graph from '../components/Graph'
 
-const Home: NextPage = () => {
-
-    interface SkillSet {
-        skill: string,
+const Skills: NextPage = () => {
+    interface Skillset {
+        title: string,
         level: number  
-    }
+    }  
 
-    const languages: SkillSet[] = [
-        {skill: 'html5', level: 99},
-        {skill: 'css, scss', level: 99},
-        {skill: 'php', level: 99},
-        {skill: 'javscript', level: 90},
-        {skill: 'typescipt', level: 35},
+    const languages: Skillset[] = [
+        {title: 'html5, css, scss', level: 90},
+        {title: 'php', level: 95},
+        {title: 'javscript', level: 90},
+        {title: 'typescipt', level: 30},
 	]
+
+    const liberies: Skillset[] = [
+        {title: 'jQuery', level: 90},
+        {title: 'react', level: 90},
+        {title: 'vue', level: 70},
+	]
+    
+    const frameworks: Skillset[] = [
+        {title: 'laravel', level: 90},
+        {title: 'nextjs', level: 80},
+        {title: 'express', level: 70},
+	]
+
+    const devops: Skillset[] = [
+        {title: 'nodeJS', level: 80},
+        {title: 'docker', level: 80},
+        {title: 'kubernetes', level: 70},
+        {title: 'gcp', level: 65},
+        {title: 'aws', level: 40},
+	]
+
+    const [activeSkillset, setActiveSkillset] = useState<Array<Skillset>>(languages)   
 
     return <>
 		<Head>
@@ -30,8 +51,15 @@ const Home: NextPage = () => {
 
 		<div className={styles.container}>
 			<UnderlinedTitle title="Skills"/>
+
+            <button className={`${styles.button} ${activeSkillset[0].title === languages[0].title && styles.active}`} onClick={() => setActiveSkillset(languages)}>languages</button>
+            <button className={`${styles.button} ${activeSkillset[0].title === liberies[0].title && styles.active}`} onClick={() => setActiveSkillset(liberies)}>liberies</button>
+            <button className={`${styles.button} ${activeSkillset[0].title === frameworks[0].title && styles.active}`} onClick={() => setActiveSkillset(frameworks)}>frameworks</button>
+            <button className={`${styles.button} ${activeSkillset[0].title === devops[0].title && styles.active}`} onClick={() => setActiveSkillset(devops)}>devops</button>
+
+            <Graph skillset={activeSkillset} />
 		</div>
 	</>
 }
 
-export default Home
+export default Skills
