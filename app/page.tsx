b1e8b1ffab5e11afdd272acafc818346d6021d2e
Jacob Juul Larsen <jacoblarsen86@live.com>
2023-04-18 20:09:28 +0200

    feat: add temp bio text

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,37 @@
 import styles from '@/styles/page.module.css';
-import { UnderlinedTitle } from '@/components/UnderlinedTitle';
 import { Contact } from '@/components/Contact';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <UnderlinedTitle title="Headline" />
+      <div className={styles.headlines}>
+        <h1>Jacob Juul Larsen</h1>
+        <h2>Web developer</h2>
+      </div>
+
+      <p>
+        As a Web developer I am very passionate tech and always tries to I am for best practice
+        methodologies.
+      </p>
+      <p>
+        I always try to work efficient and with problem-solving in mind and I have years of
+        experience creating frontend and backend scalable web application.
+      </p>
+      <p>
+        I have been writing code since I was 12 years old and, and have used varioues languages,
+        liberies and frameworks. Today my main focus is JavaScript, typscript and NodeJS.
+      </p>
+      <p>
+        I have a BA degree in E-concept development from Copenhagen school of design and technology.
+        But though my education wasn’t focused on writing code, it has always followed me, at part
+        time jobs, and since 2013 professionally as a fulltime web developer.
+      </p>
+      <p>
+        I live in Østerbro Copenhagen with my wife and our 2 boys (Otto 4 years and Villy 3 month.)
+        I like football, cycling and metal music., preferably enjoyed with a cold beer on the side.
+      </p>
 
       <Contact />
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lacinia, orci gravida
-        faucibus tristique, lectus quam accumsan turpis, sit amet mattis mauris massa in erat. Fusce
-        laoreet est et massa mollis, at aliquam nibh sollicitudin. Integer pellentesque venenatis
-        orci id euismod. Ut placerat ex at eros faucibus pharetra vitae quis ligula. Pellentesque
-        vestibulum eros a mollis tristique. Proin pharetra gravida rhoncus. Integer vel leo nec
-        libero imperdiet laoreet nec euismod metus. Sed sagittis quam eget est dignissim, non luctus
-        nulla varius. Suspendisse velit sem, lacinia ut auctor a, venenatis a justo. Duis est leo,
-        feugiat in velit nec, tincidunt dictum ligula. Nunc nec dui sed massa blandit ullamcorper.
-        Donec vehicula lectus id urna viverra vehicula. Phasellus sed velit massa. Phasellus
-        elementum lobortis enim quis hendrerit. Ut convallis ante sit amet ante volutpat
-        pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-        cubilia curae;
-      </p>
-      <p>
-        Praesent fringilla ullamcorper rutrum. Donec suscipit eu nulla vitae auctor. Nunc cursus dui
-        ex, id euismod sem rhoncus nec. Maecenas viverra fermentum accumsan. Donec tempor tincidunt
-        arcu, sit amet maximus est bibendum lobortis. Praesent condimentum massa neque. Duis ut
-        turpis porta, aliquam leo in, ullamcorper eros. Maecenas ut lorem quis arcu cursus suscipit
-        at a arcu. Nullam quis ligula non lectus tempus varius eu a massa. Phasellus euismod auctor
-        tortor vitae pretium. Nulla facilisi. Vivamus accumsan porta dui at feugiat. Quisque
-        molestie maximus nisi vel venenatis.
-      </p>
-      <p>
-        Nunc gravida ante eros, et sollicitudin justo sodales et. Vivamus auctor venenatis nunc quis
-        imperdiet. Vestibulum at semper diam, et tincidunt arcu. Cras efficitur aliquet ipsum. Nam
-        ut augue faucibus, consequat tortor ac, ornare ante. Morbi facilisis, nisi et aliquet
-        rutrum, augue mauris commodo tortor, eu congue ipsum felis dapibus orci. Integer vulputate,
-        orci non fermentum eleifend, augue ex accumsan magna, eget mattis leo ligula sit amet est.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo id dui eget
-        imperdiet. Duis vehicula dolor a risus pretium rhoncus. Cras laoreet eget nisl at tempus.
-      </p>
-      <p>
-        Fusce imperdiet, nunc blandit ullamcorper semper, risus dui ultricies dolor, ut placerat
-        magna justo eu lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam
-        finibus mattis lacus, et auctor enim facilisis vitae. Cras pulvinar, purus iaculis fringilla
-        lacinia, nulla quam bibendum ligula, id euismod libero leo id ligula. Sed viverra, est eu
-        consectetur luctus, sem purus blandit nibh, rutrum consectetur sapien libero nec diam. In
-        nec volutpat ex. In vel augue quam. Nam in enim ornare, euismod velit ut, interdum erat.
-        Suspendisse fermentum dolor ac sem efficitur, id tincidunt ex vulputate. Vestibulum tempor
-        fermentum suscipit. Quisque hendrerit ligula sit amet nunc facilisis, nec consectetur elit
-        maximus. Suspendisse vulputate sit amet mauris at aliquet. Nullam porta metus erat, sed
-        laoreet magna mollis quis. Curabitur venenatis maximus feugiat. Vestibulum non leo non ex
-        faucibus sollicitudin. Integer sit amet ipsum justo.
-      </p>
     </main>
   );
 }
